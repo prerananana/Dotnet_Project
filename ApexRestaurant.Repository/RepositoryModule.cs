0a8a85cbf19caab768342b8f9ba7d245c58a2fc2
Prerana Mali <prerana.mali@apexcollege.edu.np>
2023-02-07 17:34:13 +0545

    added api controller for customer

---
+++ ApexRestaurant.Repository/RepositoryModule.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ApexRestaurant.Repository.RCustomer;
+using MySql.EntityFrameworkCore;
+
+namespace ApexRestaurant.Repository
+{
+    public static class RepositoryModule
+    {
+        public static void Register(IServiceCollection services, string connection, string migrationsAssembly)
+        {
+            services.AddDbContext<RestaurantContext>(options => options.UseMySQL(connection, builder => builder.MigrationsAssembly(migrationsAssembly)));
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}
